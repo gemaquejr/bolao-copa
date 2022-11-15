5895f31266ac4b01fb321460a5c3b0fe53a46663
gemaquejr <gemaquejr@hotmail.com>
2022-11-15 20:48:59 -0300

    Adiciona conexão com API

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image';
+import appPreviewImg from '../assets/preview.png';
+import logoImg from '../assets/logo.png';
+import checkImg from '../assets/checked.png';
+import { api } from '../lib/axios';
+
 interface HomeProps {
   betCount: number;
+  guessCount: number;
+  userCount: number;
 }
 
-import Image from 'next/image'
-import appPreviewImg from '../assets/preview.png'
-import logoImg from '../assets/logo.png'
-import checkImg from '../assets/checked.png'
-   
 export default function Home(props: HomeProps) {
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
@@ -19,7 +22,7 @@ export default function Home(props: HomeProps) {
 
       <div className="mt-10 flex items-center gap-2">
         <strong className="text-gray-100 text-xl">
-          <span className="text-green-500">+12.592</span> pessoas já estão usando
+          <span className="text-green-500">+{props.userCount}</span> pessoas já estão usando
         </strong>
       </div>
 
@@ -56,7 +59,7 @@ export default function Home(props: HomeProps) {
         <div className="flex items-center gap-6">
           <Image src={checkImg} alt="" width={50} height={51}/>
           <div className="flex flex-col">
-            <span className="font-bold text-2xl">+2.034</span>
+            <span className="font-bold text-2xl">+{props.guessCount}</span>
             <span>Palpites enviados</span>
           </div>
         </div>
@@ -70,12 +73,17 @@ export default function Home(props: HomeProps) {
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/bets/count')
-  const data = await response.json()
+  const [betCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get('bets/count'),
+    api.get('guesses/count'),
+    api.get('users/count')
+  ])
 
   return {
     props: {
-      betCount: data.count,
+      betCount: betCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
     }
   }
 }
